5676d4b1d7aa1c01c632498853649e7bdafaf7c6
Antonis Adamou <aadamo08@ucy.ac.cy>
2026-07-09 16:07:46 +0300

    Add User DTOs classes.

---
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/UserRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿using WorkTeamProject.API.Models;
+
+namespace WorkTeamProject.API.DTOs
+{
+    public class UserRequestDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+    }
+}
---
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/UserResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿using WorkTeamProject.API.Models;
+
+namespace WorkTeamProject.API.DTOs
+{
+    public class UserResponseDTO
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+        public List<string> Roles { get; set; } = new();
+    }
+}
