2a7d00388a9025be74a3882d00c1bcb83c0f3913
Marios Adamou <madamo02@ucy.ac.cy>
2026-07-09 16:03:18 +0300

    add dtos folder with implementation of RoleRequest and RoleResponse classes

---
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/RoleRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkTeamProject.API.DTOs
+{
+    public class RoleRequest
+    {
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
---
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/RoleResponse.cs
@@ -0,0 +1,11 @@
+﻿using WorkTeamProject.API.Models;
+
+namespace WorkTeamProject.API.DTOs
+{
+    public class RoleResponse
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+        public ICollection<UserRole>? UserRoles { get; set; }
+    }
+}
