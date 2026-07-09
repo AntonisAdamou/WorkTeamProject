081fc870a3d862ae87dcf557e5349c9551819660
Marios Adamou <madamo02@ucy.ac.cy>
2026-07-09 11:59:02 +0300

    add Model folder and Role class

---
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkTeamProject.API.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
