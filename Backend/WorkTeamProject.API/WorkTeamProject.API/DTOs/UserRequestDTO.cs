218d422e96facb18d9b141ec85a61bcdbaa632c8
Antonis Adamou <aadamo08@ucy.ac.cy>
2026-07-10 11:00:00 +0300

    Replace range attribute with length in UserRequestDTO.

--- Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/UserRequestDTO.cs
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/UserRequestDTO.cs
@@ -11,7 +11,7 @@ namespace WorkTeamProject.API.DTOs
         [EmailAddress(ErrorMessage = "{0} is not a valid email address")]
         public string UserEmail { get; set; } = string.Empty;
         [Required(ErrorMessage = "Password is required.")]
-        [Range(5, 30,ErrorMessage = "Password value({0}) must be between {1} {2}")]
+        [Length(5, 30, ErrorMessage = "Password length must be between {1} and {2}")]
         public string UserPassword { get; set; } = string.Empty;
     }
 }
