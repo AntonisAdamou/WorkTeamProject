40ac3af20d8a382cc91b88f8823fad1a1b9e4818
Marios Adamou <madamo02@ucy.ac.cy>
2026-07-10 11:01:16 +0300

    fix bug in RoleRequestDTO class

--- Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/RoleRequestDTO.cs
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/RoleRequestDTO.cs
@@ -5,7 +5,7 @@ namespace WorkTeamProject.API.DTOs
     public class RoleRequestDTO
     {
         [Required(ErrorMessage = "RoleName is required")]
-        [Range(3, 15, ErrorMessage = "RoleName must be between 3 and 15 characters")]
+        [Length(3, 15, ErrorMessage = "RoleName must be between 3 and 15 characters")]
         public string RoleName { get; set; } = string.Empty;
     }
 }
