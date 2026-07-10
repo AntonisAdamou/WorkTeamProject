661beb30752730170ee5a408ffc6fb8efb059f3f
Antonis Adamou <aadamo08@ucy.ac.cy>
2026-07-10 10:52:00 +0300

    Add data annotations to UserRequestDTO.

--- Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/UserRequestDTO.cs
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/UserRequestDTO.cs
@@ -1,11 +1,17 @@
-﻿using WorkTeamProject.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using WorkTeamProject.API.Models;
 
 namespace WorkTeamProject.API.DTOs
 {
     public class UserRequestDTO
     {
+        [Required(ErrorMessage = "Name is required.")]
         public string UserName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "{0} is not a valid email address")]
         public string UserEmail { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Password is required.")]
+        [Range(5, 30,ErrorMessage = "Password value({0}) must be between {1} {2}")]
         public string UserPassword { get; set; } = string.Empty;
     }
 }
