02b39b14e0692af1852337ded08de4b63d8b3e47
Antonis Adamou <aadamo08@ucy.ac.cy>
2026-07-16 09:18:08 +0300

    Add data validation to UserRole DTO.

--- Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/UserRoleRequestDTO.cs
+++ Backend/WorkTeamProject.API/WorkTeamProject.API/DTOs/UserRoleRequestDTO.cs
@@ -4,7 +4,11 @@ namespace WorkTeamProject.API.DTOs
 {
     public class UserRoleRequestDTO
     {
+        [Required(ErrorMessage = "UserId is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "UserId must be a positive integer.")]
         public int UserId { get; set; }
+        [Required(ErrorMessage = "RoleId is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "RoleId must be a positive integer.")]
         public int RoleId { get; set; }
     }
 }
